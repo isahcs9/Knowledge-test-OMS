65cfc61402cc84885140622b9c652d0ef6731b3c
Isabella Scotta <isahcs9@gmail.com>
2020-11-25 21:33:27 -0300

    Exercício 1

--- Exe1.c
+++ Exe1.c
@@ -1,5 +1,18 @@
-double num = 0;
+#include <stdio.h>
 
-while (num < 100){
-    
+int main() {
+
+    int i = 1;
+    while (i <= 100){
+        if ((i%3 == 0) && (i%5 ==0)){
+            printf("FooBaa\n", i);    
+        } else if (i%3 == 0) {
+            printf("Foo\n", i); 
+        } else if (i%5 ==0) {
+            printf("Baa\n", i);   
+        } else {
+            printf("%d\n", i);  
+        }
+        i=i+1;
+    }
 }
